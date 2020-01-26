2787268b4b2544766cd89b53481d52f3f24882cf
satokoudai <59693685+satokoudai@users.noreply.github.com>
2020-01-27 00:26:10 +0900

    Update webserver.py

--- scripts/webserver.py
+++ scripts/webserver.py
@@ -4,9 +4,9 @@ import rospy, os
 import SimpleHTTPServer
 
 def kill():
-    os.system("kill -KILL " + str(os.getpid())) #強制シャットダウン
+    os.system("kill -KILL " + str(os.getpid())) 
 
-os.chdir(os.path.dirname(__file__))  #scriptsディレクトリが見えるように
-rospy.init_node("webserver")         #rosのノード登録
-rospy.on_shutdown(kill)              #kill関数の登録
-SimpleHTTPServer.test()              #サーバ立ち上げ
+os.chdir(os.path.dirname(__file__))  
+rospy.init_node("webserver")        
+rospy.on_shutdown(kill)              
+SimpleHTTPServer.test()              
